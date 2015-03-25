aaff06c6a1e2fe5d44b6e3a6704895887d57f7f2
teresafinn <teresa.finn@gmail.com>
2015-03-25 11:49:43 -0600

    Use filename if present

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -1,7 +1,7 @@
 # require_relative 'solver'
 
 filename = ARGV[0]
-puzzle = File.read('first_puzzle.txt')
+puzzle = File.read(filename || 'first_puzzle.txt')
 solver = Solver.new
 solution = solver.solve(puzzle)
 puts solution
